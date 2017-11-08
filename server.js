d552dbe37335738b4c40d8cdbaba1478ec213822
phytertek <ryan.phytertek@gmail.com>
2017-11-08 00:58:31 -0500

    Created server.js and config.js, initiated server with port configuration and added body-parser and cors middleware

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { PORT } = require('./config');
+
+const server = express();
+
+server.use(cors());
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(PORT, error => {
+  if (error) return console.error(error);
+  console.log(`jwt-auth-todo-api running on port ${PORT}`);
+});
