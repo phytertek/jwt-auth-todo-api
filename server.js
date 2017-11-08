63795427d2dd5f4e3315d54f30e3ef27b1e6527a
phytertek <ryan.phytertek@gmail.com>
2017-11-08 01:58:46 -0500

    Added nodemon library and development server startup script, added error handler for unspecified routes

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
+// Handle unspecified routes
+server.use((req, res) =>
+  res.status(404).json({
+    error: `Unable to resolve ${req.originalUrl}`
+  })
+);
+
 server.listen(PORT, error => {
   if (error) return console.error(error);
   console.log(`jwt-auth-todo-api running on port ${PORT}`);
